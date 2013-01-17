e4fe622099c585a06de7392e6fe355975fb578c8
Matías Waterloo <matiwater@gmail.com>
2013-01-17 03:56:28 -0300

    ya se configura el projecto en jenkins
    Conflicts:
    	manifests/lucid32.pp

--- manifests/lucid32.pp
+++ manifests/lucid32.pp
@@ -1,5 +1,6 @@
 class { 'gradle':
   version => '1.3',
+  timeout => 1500, 
 }
 
 package{"unzip":
@@ -9,6 +10,59 @@ package{"unzip":
  
 package{"git-core":
     ensure=>"installed"
+}
+
+user { "jenkins":
+   ensure => 'present',
+   managehome=>true,
+}
+
+file { "gradle-jenkins":
+  path	   => "/var/lib/jenkins/hudson.plugins.gradle.Gradle.xml",
+  source   => '/vagrant/jenkins_files/hudson.plugins.gradle.Gradle.xml',
+  ensure   => "present",
+  owner	   => 'jenkins',
+  group    => 'jenkins',
+  require => [Class['gradle'],Jenkins::Plugin['gradle']],
+  notify   => Service['jenkins'],
+}
+
+file { "git-jenkins":
+  path	   => "/var/lib/jenkins/hudson.plugins.git.GitSCM.xml",
+  source   => '/vagrant/jenkins_files/hudson.plugins.git.GitSCM.xml',
+  ensure   => "present",
+  owner	   => 'jenkins',
+  group    => 'jenkins',
+  require => [Package['git-core'],Jenkins::Plugin['git']],
+  notify   => Service['jenkins'],
+}
+
+file { "jenkins-jobs":
+  path	   => "/var/lib/jenkins/jobs",
+  ensure   => "directory",
+  owner	   => 'jenkins',
+  group    => 'jenkins',
+  require => [File['git-jenkins'],File['gradle-jenkins']],
+  notify   => Service['jenkins'],
+}
+
+file { "jenkins-project":
+  path	   => "/var/lib/jenkins/jobs/prueba",
+  ensure   => "directory",
+  owner	   => 'jenkins',
+  group    => 'jenkins',
+  require => File['jenkins-jobs'],
+  notify   => Service['jenkins'],
+}
+
+file { "jenkins-project-config":
+  path	   => "/var/lib/jenkins/jobs/prueba/config.xml",
+  source   => '/vagrant/jenkins_files/config.xml',
+  ensure   => "present",
+  owner	   => 'jenkins',
+  group    => 'jenkins',
+  require => File['jenkins-project'],
+  notify   => Service['jenkins'],
 }
 
 jenkins::plugin {
